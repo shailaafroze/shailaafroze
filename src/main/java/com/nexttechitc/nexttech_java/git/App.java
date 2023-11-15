81589166df0299a96c1c9ab1456441fa71fed2fd
shila <shaila@afroze.com>
2023-11-15 15:50:45 -0800

    test

---
+++ src/main/java/com/nexttechitc/nexttech_java/git/App.java
@@ -0,0 +1,13 @@
+package com.nexttechitc.nexttech_java.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
